be2d688da9789dedd873ca01243a4f122ed039c5
Florian Munz <surf@theflow.de>
2013-08-21 16:47:26 +0200

    use correct sh in test

--- tests/actor_test.rb
+++ tests/actor_test.rb
@@ -51,7 +51,7 @@ class ActorTest < Test::Unit::TestCase
   private
     def setup_tmp_backup_dir
       newtmp = @config[:tmp_dir] + "/backup_#{rand}_" + Time.now.strftime("%Y%m%d%H%M%S")
-      sh "mkdir #{newtmp}"
+      Rake::FileUtilsExt.sh "mkdir #{newtmp}"
       config = <<-END
         set :backup_path, "#{newtmp}" 
       END
@@ -60,8 +60,8 @@ class ActorTest < Test::Unit::TestCase
 
     def create_tmp_files
       newtmp = @config[:tmp_dir] + "/test_#{rand}_" + Time.now.strftime("%Y%m%d%H%M%S")
-      sh "mkdir #{newtmp}"
-      0.upto(5) { |i| sh "touch #{newtmp}/#{i}" }
+      Rake::FileUtilsExt.sh "mkdir #{newtmp}"
+      0.upto(5) { |i| Rake::FileUtilsExt.sh "touch #{newtmp}/#{i}" }
       newtmp
     end
 
